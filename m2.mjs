685f975e2d3ab50015077853f4ab68782de95345
hafsa-bgr <hafsa.boughir@gmail.com>
2024-09-26 20:36:10 +0200

    m2.mjs

---
+++ m2.mjs
@@ -0,0 +1,8 @@
+let geboorteJaar = 2004;  
+let toekomstJaar = 2048;   
+
+let leeftijd1 = toekomstJaar - geboorteJaar;
+
+let leeftijd2 = leeftijd1 - 1;
+
+console.log("In " + toekomstJaar + " zal ik " + leeftijd2 + " of " + leeftijd1 + " jaar oud zijn.");
